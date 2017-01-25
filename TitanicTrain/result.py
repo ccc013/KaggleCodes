46378cad6f06bc6627ff6234205705b0d1dfff89
cai <429546420@qq.com>
2017-01-25 16:29:33 +0800

    modified

--- TitanicTrain/result.py
+++ TitanicTrain/result.py
@@ -11,7 +11,7 @@ import pandas as pd
 import os
 from pandas import Series,DataFrame
 import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
 
 rootPath = "/home/cai/KaggleCodes/TitanicTrain"
 trainPath = os.path.join(rootPath, "train.csv")
@@ -114,12 +114,37 @@ df_test = pd.concat([data_test, dummies_Cabin, dummies_Embarked, dummies_Sex, du
 df_test.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
 df_test['Age_scaled'] = scaler.fit_transform(df_test['Age'], age_scale_param)
 df_test['Fare_scaled'] = scaler.fit_transform(df_test['Fare'], fare_scale_param)
-print(df_test)
-test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
-predictions = clf.predict(test)
+#print(df_test)
+#==============================================================================
+# test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+# predictions = clf.predict(test)
+# result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions.astype(np.int32)})
+# resultPath = os.path.join(rootPath,"logistic_regression_predictions.csv")
+# result.to_csv(resultPath, index=False)
+#==============================================================================
+
+
+
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass.*|Mother|Child|Family|Title')
+train_np = train_df.as_matrix()
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到BaggingRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+bagging_clf = BaggingRegressor(clf, n_estimators=20, max_samples=0.8, max_features=1.0, bootstrap=True, bootstrap_features=False, n_jobs=-1)
+bagging_clf.fit(X, y)
+
+test = df_test.filter(regex='Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass.*|Mother|Child|Family|Title')
+predictions = bagging_clf.predict(test)
 result = pd.DataFrame({'PassengerId':data_test['PassengerId'].as_matrix(), 'Survived':predictions.astype(np.int32)})
-resultPath = os.path.join(rootPath,"logistic_regression_predictions.csv")
-result.to_csv(resultPath, index=False)
+baggingResultPath = os.path.join(rootPath,"logistic_regression_bagging_predictions.csv")
+result.to_csv(baggingResultPath, index=False)
+
 
 # show full train data format
 #print(data_train)
